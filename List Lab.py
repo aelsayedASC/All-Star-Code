798e303a0af01aaafc15e25b7f8c1d09c7573bf9
aelsayedASC <auelsayed@gmail.com>
2016-07-15 10:53:29 -0400

    Lab of list questions

---
+++ List Lab.py
@@ -0,0 +1,22 @@
+listA=[10,11,12,13,14,15,16,17,18,19,20]
+listB=[0,0,0,0,0]
+listC=[]
+listD=[]
+i=0
+while i<4:
+    listD.append(0)
+    i=i+1
+listE=["a",5,"doodle",3,10]
+print(len(listE))
+listE.remove("doodle")
+listE.append(20)
+listE[0]=8.4
+listE.insert(0,"hi mom")
+print(listE)
+listF=[]
+i=1
+while i<10:
+    listF.append(i)
+    i=i+2
+print(listF)
+    
