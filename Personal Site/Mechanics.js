78f3e81e651da852487412b750c5372ee1351463
aelsayedASC <auelsayed@gmail.com>
2016-07-29 10:49:54 -0400

    Updated version

--- Personal Site/Mechanics.js
+++ Personal Site/Mechanics.js
@@ -8,7 +8,18 @@ $(function() {
           scrollTop: target.offset().top
         }, 1000);
         return false;
+          
       }
     }
   });
 });
+$("#myTopnav").hide();
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+myFunction()
